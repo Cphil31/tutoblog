1e32ce2dcd6861bcf82ea99d85ec86ce9f9a7896
Cphil31 <cecilephilippe31@gmail.com>
2018-05-12 17:52:41 +0200

    remplir la base de données a l'aide du model

--- database/migrations/2018_05_12_150515_create_users_table.php
+++ database/migrations/2018_05_12_150515_create_users_table.php
@@ -22,6 +22,11 @@ class CreateUsersTable extends Migration
             $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
+
+        Users::create([
+            'username'=>'Philippe',
+            
+        ])
     }
 
     /**
