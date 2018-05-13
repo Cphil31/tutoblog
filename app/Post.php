a0e0cb3f1f65c6425ddb1dbdd061a9d58ec1aa84
Cphil31 <cecilephilippe31@gmail.com>
2018-05-13 16:43:42 +0200

    lier les tables : model post  : un post appartient a un utilisateur et il peut avoir plusieurs commentaires

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,12 @@ class Post extends Model
 {
     // les champs que l'on ne peut pas modifer
     protected $guarded = ['id','created_at'];
+
+    public function user(){
+    	return $this->belongsto('User');
+    }
+
+    public function comments(){
+    	return $this->hasMany('Comment');
+    }
 }
