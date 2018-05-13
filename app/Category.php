886f477755a107dbf8cd9a0fc5624501c3cfb45a
Cphil31 <cecilephilippe31@gmail.com>
2018-05-13 16:31:41 +0200

    lier les tables :une categorie plusieurs posts

--- app/Category.php
+++ app/Category.php
@@ -8,4 +8,10 @@ class Category extends Model
 {
     // les champs que l'on ne peut pas modifer
     protected $guarded = ['id','created_at'];
+   // Une catégorie peu avoir plusieurs posts 
+    public function posts(){
+
+    	return $this->hasMany('Post');
     }
+
+  }
