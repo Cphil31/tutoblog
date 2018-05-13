0302512c1fad637a13b88ec33e87c43ba23cee58
Cphil31 <cecilephilippe31@gmail.com>
2018-05-13 16:38:58 +0200

    lier les tables : model comment :un commentaire appartien a 1 post et a 1 user

--- app/Comment.php
+++ app/Comment.php
@@ -8,4 +8,12 @@ class Comment extends Model
 {
     // les champs que l'on ne peut pas modifer
     protected $guarded = ['id','created_at'];
+
+    public function post(){
+    	return $this->belongsTo('Post');
+    }
+
+    public function user(){
+    	return $this->belongsTo('Users');
+    }
 }
